8d8957653bcd7619ca945943f2b9035b2db3be42
Franceschini-Bambou <bambou.franceschini@edu.univ-fcomte.fr>
2026-03-10 08:38:30 +0100

    backend

--- src/js/test-back.js
+++
@@ -1,9 +0,0 @@
-import {artistesSorted} from './backend.mjs';
-
-
-try {
-    const records = await artistesSorted();
-    console.log(JSON.stringify(records, null, 2));
-} catch (e) {
-    console.error(e);
-}
